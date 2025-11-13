c7db63b3ed61e574b831e0a0579b86be1bf8d563
Cap3ya <capeya92@gmail.com>
2025-11-13 15:01:28 +0100

    add period setting + volume filter

--- lib/config.py
+++ lib/config.py
@@ -10,6 +10,9 @@ class Config:
     CSV_FILE = os.path.join(OUTPUT_DIR, "stockScreener.csv")
     BAD_TICKERS_FILE = os.path.join(OUTPUT_DIR, "stockScreener_badTickers.txt")
 
+    PERIOD_DAYS = 63
+    MIN_DOLLAR_VOLUME = 1_000_000  
+
     @classmethod
     def ensure_output_dir(cls):
         os.makedirs(cls.OUTPUT_DIR, exist_ok=True)
